79549f86c902411cb2f4ac570b5293bdbc1f71ea
Jeanette <jkootinsanwu@gmail.com>
2020-11-30 07:20:38 -0500

    updates

--- Assets/js/deadScene.js
+++ Assets/js/deadScene.js
@@ -4,29 +4,63 @@ class deadScene extends Phaser.Scene{
     var playerScore; 
   }
 
-  preload(){
-    //load background land and sky
-    this.load.image('skynight', 'assets/images/LoveUpper-night.png');
-    this.load.image('skyday', 'assets/images/LoveUpper.png');
-    this.load.image('sunclear', 'assets/images/day-layer.png');
-    this.load.image('suncloudy', 'assets/images/day-layer-cloudy.png');
-    this.load.image('moonclear', 'assets/images/night-layer.png');
-    this.load.image('suncloudy', 'assets/images/night-layer-cloudy.png');
-    this.load.image('cloud1', 'assets/images/cloud1.png');
-    this.load.image('cloud2', 'assets/images/cloud2.png');
-    this.load.image('cloud3', 'assets/images/cloud3.png');
-    this.load.image('replay', 'assets/images/replay-button.png');
+  function startGame(){
+    document.getElementById('Level1').style.display = 'none';
   }
-  
-  create(){
-    this.scoreLabel = this.add.bitmapText(250, config.height /2 - 100, "pixelFont", "SCORE: " + score, 200);
-    
+  function replay(){
+    document.getElementById('deadScene').style.display = 'none';
+    buttonGo.style.display = 'none';
+    replayButton = 'replay';
+    Image.Computer.src = 'images/replay-button.png';
+    document.getElementById.style.backgroundColor = 'yellow';
+
+    deselctAll();
+    location.reload();
   }
-  
-  update(){
-    this.dissappearTimer ++;
-    if(this.dissappearTimer >= 1000){this.destroy();}
+  function go(){
+    var textEndTitle = document.getElementById('textEndTitle');
+    var textEndMessage = document.getElementById('textEndMessage');
+  }
+  if (points == 0) {
+    if(playerScore = 1000) {
+      level = 'Level1Boss';
+    }
+    else if(playerScore = 2000) {
+      level = 'Level2Boss';
+    }
+    else (playerScore = 3000) => {
+      level = 'Level3Boss';
+    }
+  }
+
+  document.getElementById('endScreen').style.display ='block';
+
+  //graphics
+  var graphics = this.add.graphics();
+  graphics.fillStyle(0x000000, 1);
+  graphics.beginPath();
+  graphics.moveTo(0,0);
+  graphics.lineTo(config.width, 0);
+  graphics.lineTo(config.width, 80);
+  graphics.lineTo(0, 80);
+  graphics.lineTo(0, 0);
+  graphics.closePath();
+  graphics.fillPath();
 
 
-  }
 }
+  //preload(){
+    
+  //}
+  
+  //create(){
+    //this.scoreLabel = this.add.bitmapText(250, config.height /2 - 100, "pixelFont", "SCORE: " + score, 200);
+    
+  //}
+  
+  //update(){
+    //this.dissappearTimer ++;
+    //if(this.dissappearTimer >= 1000){this.destroy();}
+
+
+  //}
