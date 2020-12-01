37e70ebc10d6006afbc90d4aeaa96408634e665d
defiantturtle <sarabarnes078@gmail.com>
2020-12-01 16:58:29 -0500

    Centering the loading icon and word

--- Assets/js/LoadingScene.js
+++ Assets/js/LoadingScene.js
@@ -8,9 +8,23 @@ class LoadingScene extends Phaser.Scene{
         this.load.image('loadBase', 'assets/images/LoadingBase.png');
 
         //Loading icon and word spritesheets
-        this.load.spritesheet('loadIcon', 'assets/images/LoadingSprite.png');
-        this.load.spritesheet('loadWord', 'assets/images/LoadingWordSprite.png');
+        this.load.spritesheet('loadIcon', 'assets/images/LoadingSprite.png', {
+            frameWidth: 640,
+            frameHeight: 540,
+        });
+        this.load.spritesheet('loadWord', 'assets/images/LoadingWordSprite.png', {
+            frameWidth: 337,
+            frameHeight: 45
+        });
 
+        //Positioning icon and word
+        this.loadIcon = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'loadIcon');
+        this.logo.anchor.setTo(0.5);
+        this.loadWord = this.add.sprite(this.game.world.centerX, this.game.centerY + 145, 'loadWord');
+        this.loadWord.anchor.setTo(0.5);
+    }
+
+    create(){
         
     }
 }
