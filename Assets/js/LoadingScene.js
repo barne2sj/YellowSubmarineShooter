14f9a1fbac0513cb7756001cccde7e2856398ef0
defiantturtle <sarabarnes078@gmail.com>
2020-12-01 15:47:02 -0500

    Added loading background and sprites

--- Assets/js/LoadingScene.js
+++ Assets/js/LoadingScene.js
@@ -1,42 +1,16 @@
-// class Scene1 extends Phaser.Scene {
-//     constructor() {
-//       super("playGame");
-//     }
-    
-    
-//     create() {
-//         skyTile = this.add.tileSprite(0,0,config.width, config.height, "sky");
-//         groundTile = this.add.tileSprite(0,0,config.width, config.height, "land");
+class LoadingScene extends Phaser.Scene{
+    constructor(){
+        super("LoadingScene");
+    }
 
+    preload(){
+        //Background
+        this.load.image('loadBase', 'assets/images/LoadingBase.png');
 
-//       //  this.handintro1 = this.add.sprite(config.width/2 - 50, 0, "handboss");
-//         //this.handintro1.play("handintro");
-//       //  this.moveBoss(hand);
+        //Loading icon and word spritesheets
+        this.load.spritesheet('loadIcon', 'assets/images/LoadingSprite.png');
+        this.load.spritesheet('loadWord', 'assets/images/LoadingWordSprite.png');
 
-//     }
-
-//     moveBoss(boss, speed) {
-//         // if(boss.y <= config.height/2){
-//         //     boss.y += speed; 
-//         // }
-//         // if(boss.x <= config.width){
-//         //     boss.x += speed;
-//         // } else {
-//         //     this.resetBoss(boss);
-//         // }
         
-        
-//     }
-
-//     resetBoss(boss) {
-//         boss.x = 0;
-//         boss.y = 0;
-//     }
-
-//     update()   {
-//         //this.moveBoss(this.bossIntro1, 2);
-//         this.moveBackground(null, null, groundTile, 0, skytile, 0.5);
-//     }
-
-
-// }
+    }
+}
